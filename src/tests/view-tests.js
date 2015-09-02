36558845e68068f854379d1fba2037e1b383c1e8
toshko <toshkooooooooooo@gmail.com>
2015-09-02 19:00:23 +0300

    add view.draw() test

--- src/tests/view-tests.js
+++ src/tests/view-tests.js
@@ -57,29 +57,85 @@ describe('View tests', function () {
     });
 
     describe('View function ', function () {
-		it('reset() shuold reset view.cells background color to rgb(204, 204, 204)', function(){
-			
-			var grid = [];
-			 for (var i = 0; i < 18; i+=1) {
-				 grid[i] = new Array(12);
-            	for (var j = 1; j < 11; j+=1) {
-					grid[i][j] = $('<a/>').css('backgroundColor', 'black');
-            	}
-        	}
-			
-			view.cells = grid;
-			view.reset(grid);
-			
-			var counter = 0;
-			for (var index = 0, len = view.cells.length; index < len; index++) {
-				for (var j = 1; j < 11; j+=1) {
-					if(grid[index][j].css('backgroundColor') === 'rgb(204, 204, 204)'){
-						counter += 1;
-					}
-            	}	
-			}
+        it('reset() shuold reset view.cells background color to rgb(204, 204, 204)', function () {
+            // Arrange
+            var grid = [];
+            for (var i = 0; i < 18; i += 1) {
+                grid[i] = new Array(12);
+                for (var j = 1; j < 11; j += 1) {
+                    grid[i][j] = $('<a/>').css('backgroundColor', 'black');
+                }
+            }
 
-			expect(counter).to.be.eql(180);
-		})
+            view.cells = grid;
+            // Act
+            view.reset(grid);
+
+            var counter = 0;
+            for (var index = 0, len = view.cells.length; index < len; index++) {
+                for (var j = 1; j < 11; j += 1) {
+                    if (view.cells[index][j].css('backgroundColor') === 'rgb(204, 204, 204)') {
+                        counter += 1;
+                    }
+                }
+            }
+
+            // Assert
+            expect(counter).to.be.eql(180);
+        })
+        
+        it('draw() should draw correct current shape', function () {
+            // Arrange
+            var i, j, currentShape = [
+                [
+                    [0, 0, 0, 0],
+                    [1, 1, 1, 0],
+                    [0, 1, 0, 0],
+                    [0, 0, 0, 0]
+                ],
+                [],
+                [],
+                []
+            ];
+
+            var currentShapeNotEmptyCells = 0;
+            currentShape[0]
+                .forEach(function (arr) {
+                    arr.forEach(function (num) {
+                        if (num) {
+                            currentShapeNotEmptyCells += 1;
+                        }
+                    })
+                });
+
+            var grid = [];
+            for (i = -3; i < 18; i += 1) {
+                grid[i] = [];
+                for (j = 1; j < 11; j += 1) {
+                    grid[i][j] = $('<a />');
+                }
+            }
+
+            view.cells = grid;
+            var shapePosition = 0;
+            var color = 'rgb(234, 234, 234)';
+            var xPos = 4;
+            var yPos = -3;
+            var counter = 0;
+
+            // Act
+            view.draw(currentShape, shapePosition, xPos, yPos, color);
+
+            for (i = -3; i < 18; ++i) {
+                for (j = 1; j < 11; ++j) {
+                    if (view.cells[i][j].css('backgroundColor') === 'rgb(234, 234, 234)') {
+                        counter += 1;
+                    }
+                }
+            }
+            
+            // Assert
+            expect(counter).to.be.eql(currentShapeNotEmptyCells);
+        })
     })
 });
