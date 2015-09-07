be11d58c1be4f74c1ee22f64fafb553be67b9993
NayaIt <n.m.keranova@gmail.com>
2015-09-07 16:17:55 +0300

    Upload code about Local Storage (Player's name and score saved in local storage)

--- src/scripts/score.js
+++ src/scripts/score.js
@@ -1,1 +1,37 @@
-// todo
+//Player's name and score saved in local storage
+
+//var scoreObject = (function(){
+//    'use strict';
+//
+//    var score = 0;
+//
+//    function initScore() {
+//        score = 0;
+//    }
+//
+//    function updateScore(points) {
+//        score = points;
+//    }
+//
+//    function writeScore() {
+//
+////prompt the user for name
+//        var username = prompt('Please enter your name:');
+//
+//        var scores = JSON.parse(localStorage['scores']) || [];
+//        scores.push({username: username, score: score});
+//        localStorage['scores'] = JSON.stringify(scores);
+//    }
+//
+//    function readScore() {
+//        var scores = JSON.parse(localStorage['scores']) || [];
+//        return scores;
+//    }
+//
+//    return {
+//        initScore: initScore,
+//        updateScore: updateScore,
+//        writeScore: writeScore,
+//        readScore: readScore
+//    }
+//}());
